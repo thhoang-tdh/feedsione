70c11a1106d1e3832c409a608f9a284d467e567e
Thanh Hoang <tdhoangdev@gmail.com>
2022-06-08 20:54:42 -0700

    testing

--- config/settings/base.py
+++ config/settings/base.py
@@ -290,9 +290,12 @@ ACCOUNT_ALLOW_REGISTRATION = env.bool("DJANGO_ACCOUNT_ALLOW_REGISTRATION", True)
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_AUTHENTICATION_METHOD = "username"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
-ACCOUNT_EMAIL_REQUIRED = True
+# ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_REQUIRED = False
+
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
-ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_EMAIL_VERIFICATION = "None"
+
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_ADAPTER = "feedsione.users.adapters.AccountAdapter"
 # https://django-allauth.readthedocs.io/en/latest/forms.html
