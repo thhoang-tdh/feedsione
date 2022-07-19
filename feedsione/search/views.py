40bd4d83eba82ea3b3f446921193ff48cc8a06f6
Thanh Hoang <tdhoangdev@gmail.com>
2022-07-18 17:32:25 -0700

    updated

--- feedsione/search/views.py
+++ feedsione/search/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from feedsione.news.models import *
 from django.views.generic import ListView, DetailView, CreateView
-from django.db.models import Q
+from django.db.models import Q, Prefetch
 from django.contrib.postgres.search import (
     SearchVector, SearchQuery, SearchRank, SearchHeadline
 )
 from feedsione.search.forms import SearchForm
+
 
 
 class SearchFeedResultsView(ListView):
@@ -23,23 +24,26 @@ class SearchFeedResultsView(ListView):
 
         q = form.cleaned_data.get('q')
         if q.lower() == "all":
-            return Feed.objects.all()
+            qs =  Feed.objects.all()
+        else:
+            query = SearchQuery(q)
+            vector = SearchVector('title', 'feed_url')
+            qs = Feed.objects.annotate(
+                rank = SearchRank(vector, query)
+            ).filter(rank__gt=0).order_by('-rank')
 
-        query = SearchQuery(q)
-        vector = SearchVector('title', 'feed_url')
-        object_list = Feed.objects.annotate(
-            rank = SearchRank(vector, query)
-        ).filter(rank__gt=0).order_by('-rank')
+            if not qs or len(qs) == 0:
+                # partial words, ..
+                qs = Feed.objects.filter(
+                                Q(title__icontains=q) |
+                                Q(feed_url__icontains=q) |
+                                Q(site_url__icontains=q)
+                            )
 
-        if object_list:
-            return object_list.prefetch_related('folders')
+        return qs.prefetch_related(Prefetch('folders',
+                        queryset=Folder.objects.filter(user=self.request.user)))
 
-        # partial words, ..
-        return  Feed.objects.filter(
-            Q(title__icontains=q) |
-            Q(feed_url__icontains=q) |
-            Q(site_url__icontains=q)
-        )
+
 
 
     def get_context_data(self, *arg, **kwargs):
